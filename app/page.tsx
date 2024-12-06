534312a0ad273d8bbdb0c7b1a1e44db37dc19479
Laduve-prog <duvernay.benoit@gmail.com>
2024-12-06 01:06:08 +0100

    Ajout de la surpervision via sentry

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import ExpandingArrow from '@/components/expanding-arrow'
 
 export const dynamic = 'force-dynamic'
 
+
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center">
